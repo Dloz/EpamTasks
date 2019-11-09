3688ebbda3f7cb429d21e19c3f31870d3bbfd023
Dloz <lozovik.dmitry@gmail.com>
2019-11-09 18:30:45 +0300

    first commit

---
+++ Task0/Task0/Player/WindowsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task0.Codecs.Factory;
+
+namespace Task0.MediaLibrary.Players
+{
+    class WindowsPlayer : IPlayer
+    {
+        public void Play(IEnumerable<IMediaElement> mediaElements)
+        {
+            foreach (var element in mediaElements)
+            {
+                new CodecFactory().CreateCodec(element.FileExtension).Process(element);
+            }
+        }
+
+        public void Play(IEnumerable<IPlaylist> playlists)
+        {
+            foreach (var playlist in playlists)
+            {
+                Play(playlist.Elements.AsEnumerable());
+            }
+        }
+    }
+}
